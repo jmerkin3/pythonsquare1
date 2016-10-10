a886ddf5568a62a426208cef1768a39282f95089
Joey Merkin <jmerkin@chadwickschool.org>
2016-10-10 10:21:28 -0700

    added stuff such as functions and def

--- square.py
+++ square.py
@@ -1,7 +1,18 @@
-#first line
+#importing Myro robot commands and opening simulator
 from Myro import *
 init("sim") # if sim is not already running
 # that was just set up now starting movement commands
+
+def sqside1(): #moving robot in big square
+    penDown()
+    wait(.5)
+    forward(1,8)
+    penUp()
+    turnBy(270)
+    
+def sqside2(): #smaller square
+    #smaller
+    
 
 penUp()
 backward(1,1.5)
@@ -13,22 +24,22 @@ forward(1,1)
 forward(1,1)
 wait(1)
 turnBy(270)
-penDown()
-wait(.5)
-forward(1,8)
-penUp()
-turnBy(270)
-penDown()
-forward(1,8)
-penUp()
-turnBy(270)
-penDown()
-forward(1,8)
-penUp()
-turnBy(270)
-penDown()
-forward(1,8)
-penUp()
+
+#about to draw big swuare
+sqside1()
+sqside1()
+sqside1()
+sqside1()
+
+#about to draw small square
+sqside2()
+sqside2()
+sqside2()
+sqside2()
+
+
+#
+init("sim")
 turnBy(225)
 forward(1,1.8)
 turnBy(45)
